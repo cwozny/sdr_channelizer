16a685163e3109a620063809505b3888819e2c96
Chris Wozny <1447143+cwozny@users.noreply.github.com>
2023-09-09 07:41:58 -0400

    latency is killing ability to keep track of main beam

--- cpp/usrp_main_beam_tracker.cpp
+++ cpp/usrp_main_beam_tracker.cpp
@@ -11,6 +11,7 @@
 
 #include <cstring>
 #include <ctime>
+#include <cmath>
 
 #include <bit>
 #include <iostream>
@@ -20,6 +21,35 @@
 #include <vector>
 #include <iterator>
 #include <execution>
+
+#include <Eigen/Dense>
+#include <Eigen/QR>
+
+const double get_main_beam_peak_time(const std::vector<double> &t, const std::vector<double> &v)
+{
+	// Create Matrix Placeholder of size n x k, n = number of datapoints, k = order of polynomial, for example k = 3 for cubic polynomial
+	Eigen::MatrixXd T(t.size(), 3);
+	Eigen::VectorXd V = Eigen::VectorXd::Map(&v.front(), v.size());
+	Eigen::Vector3d p;
+
+	// check to make sure inputs are correct
+	assert(t.size() == v.size());
+	assert(t.size() >= 3);
+
+	// Populate the matrix
+	for(size_t i = 0 ; i < t.size(); ++i)
+	{
+		for(size_t j = 0; j < 3; ++j)
+		{
+			T(i, j) = pow(t.at(i), j);
+		}
+	}
+
+	// Solve for linear least square fit
+	p = T.householderQr().solve(V);
+
+	return (-p[1]/(2*p[2])); // this represents the peak of the main beam as estimated by a quadratic polynomial fit
+}
 
 void getFilenameStr(char* filenameStr)
 {
@@ -66,6 +96,9 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 	std::int32_t rxGain = atoi(argv[4]);
 	const float dwellDuration = atof(argv[5]);
 	const float collectionDuration = atof(argv[6]);
+
+	std::vector<double> mbeTimeList;
+	double nextMbeTime = 0;
 
 	//create a usrp device
 
@@ -198,7 +231,22 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 
 		size_t num_accum_samps = 0;
 
+		if (nextMbeTime > 0)
+		{
+			//std::cout << "Scheduling next RX for " << nextMbeTime - (dwellDuration/2) << std::endl;
+			stream_cmd.stream_now  = false;
+			stream_cmd.time_spec   = uhd::time_spec_t(nextMbeTime - (dwellDuration/2));
+			nextMbeTime = 0;
+		}
+		else
+		{
+			//std::cout << "Missed MBE, scheduling next RX for now" << std::endl;
+			stream_cmd.stream_now  = true;
+			stream_cmd.time_spec   = uhd::time_spec_t();
+		}
+
 		stream_cmd.stream_mode = uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS;
+
 		rx_stream->issue_stream_cmd(stream_cmd);
 
 		while(num_accum_samps < sampleLength)
@@ -206,7 +254,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 			const std::int32_t startIndex = num_accum_samps;
 			const std::int32_t remainingSize = sampleLength-num_accum_samps;
 
-			num_accum_samps += rx_stream->recv(&iq[startIndex], remainingSize, meta, 5.0, true);
+			num_accum_samps += rx_stream->recv(&iq[startIndex], remainingSize, meta, 30.0, true);
 
 			// Handle streaming error codes
 			switch (meta.error_code)
@@ -234,37 +282,31 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 		}
 
 		stream_cmd.stream_mode = uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
+		stream_cmd.stream_now  = true;
+		stream_cmd.time_spec   = uhd::time_spec_t();
 		rx_stream->issue_stream_cmd(stream_cmd);
-
-		std::cout << "Received " << num_accum_samps << std::endl;
 
 		// If we received a full set of samples with no error
 		if (badSamples == false)
 		{
-			// Look for instances of saturating to min or max value
-			//const auto [minSamp, maxSamp] = std::minmax_element(std::execution::par_unseq, std::begin(iq_vec), std::end(iq_vec));
-
-			//saturated = (((*minSamp) <= SAMP_MIN) || ((*maxSamp) >= SAMP_MAX));
-
 			for(std::uint32_t jj = 0; jj < num_accum_samps; jj++)
 			{
 				mag[jj] = std::abs(iq[jj]);
-				phase[jj] = std::arg(iq[jj]);
 			}
 
 			std::sort(std::execution::par_unseq, mag.begin(), mag.end());
 
 			// Compute the noise floor as the median of the magnitude of the I/Q data
-			const float NOISE_FLOOR = mag[mag.size()/2]; // compute the median by picking the middle element of sorted vector
-			const float SNR_THRESHOLD = 20; // dB
-			const float PULSE_THRESHOLD = NOISE_FLOOR * pow(10,SNR_THRESHOLD/10);
-
-			std::cout << "Noise floor = " << NOISE_FLOOR << std::endl;
-			std::cout << "Pulse threshold = " << PULSE_THRESHOLD << std::endl;
+			const double NOISE_FLOOR = mag[mag.size()/2]; // compute the median by picking the middle element of sorted vector
+			const double SNR_THRESHOLD = 20; // dB
+			const double PULSE_THRESHOLD = NOISE_FLOOR * pow(10,SNR_THRESHOLD/10);
 
 			bool pulseActive = false; // keeps track of whether pulse is active
 			bool pulseSaturated = false; // keeps track of whether pulse was ever saturated
 			std::uint32_t toa = 0; // this is sufficient as long as we don't ever record more than ~35 seconds of samples at 60 Msps in one buffer
+			double amp = 0;
+			std::vector<double> toaList;
+			std::vector<double> snrList;
 
 			// Loop through I/Q and generate PDWs
 			for(std::uint32_t jj = 0; jj < num_accum_samps; jj++)
@@ -277,6 +319,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 						pulseActive = true; // a pulse is now active
 						toa = jj; // initialize the time of arrival to current index
 						pulseSaturated = false; // initialize whether the pulse was ever saturated
+						amp = std::abs(iq[jj]);
 					}
 				}
 				else // Look for a trailing edge now that pulse is active
@@ -286,44 +329,25 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 						pulseActive = false; // the pulse is no longer active
 
 						// compute the UTC time of the time of arrival of the pulse
-						double thisToa = (toa/fs)+meta.time_spec.get_real_secs();
+						const double thisToa = (toa/fs);
+						toaList.push_back(thisToa);
 
-						// compute the amplitude as the median magnitude over the entire pulse
-						//thisAmp = median(mag(toa:jj));
+						// compute the amplitude as the mean magnitude over the entire pulse
+						amp /= (jj-toa);
 
 						// compute the SNR for this pulse given the
 						// amplitude and noise floor for this channelizer bin
-						//thisSnr = 10*log10(thisAmp/NOISE_FLOOR);
-
-						// compute the pulse width as the number of samples
-						// this pulse was active for divided by the sampling
-						// rate for this channelizer bin
-						double thisPw = (jj-toa)/fs;
-
-						// compute the median phase difference of this pulse
-						// in order to compute the frequency
-						//phaseDiff = diff(phase(toa:jj));
-						//phaseDiff(phaseDiff < -180) = phaseDiff(phaseDiff < -180) + 360;
-						//phaseDiff(phaseDiff > 180) = phaseDiff(phaseDiff > 180) - 360;
-						//medPhaseDiff = median(phaseDiff);
-
-						// compute the frequency by finding the period given
-						// the median phase difference and then offset it
-						// from the center frequency of this channelizer bin
-						//thisFreq = fc+(fs/(360/medPhaseDiff));
-
-						std::cout << thisToa << " " << thisPw << std::endl;
-						//pdw.toa = [pdw.toa; thisToa];
-						//pdw.freq = [pdw.freq; thisFreq];
-						//pdw.pw = [pdw.pw; thisPw];
-						//pdw.snr = [pdw.snr; thisSnr];
-						//pdw.sat = [pdw.sat; pulseSaturated];
+						const double thisSnr = 10*log10(amp/NOISE_FLOOR);
+						snrList.push_back(thisSnr);
 					}
 					else // Otherwise we're still measuring a pulse
 					{
+						amp += std::abs(iq[jj]); // continue accumulating the magnitude of the pulse
+
 						if (std::abs(iq[jj].real()) >= SAMP_MAX || std::abs(iq[jj].imag()) >= SAMP_MAX)
 						{
 							pulseSaturated = true;
+							saturated = true;
 						}
 					}
 				}
@@ -332,20 +356,33 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 			// Now that we've generated the PDWs, let's go through and see if a
 			// main beam event occurred
 
-			// p = polyfit(pdw.toa,pdw.snr,2);
-			// t1 = min(pdw.toa):0.001:max(pdw.toa);
-			// y1 = polyval(p,t1);
+			if (toaList.size() > 10)
+			{
+				const double thisMbeTime = get_main_beam_peak_time(toaList, snrList) + meta.time_spec.get_real_secs();
+				std::cout << std::setprecision(15) << "MBE was " << thisMbeTime << std::endl;
+				mbeTimeList.push_back(thisMbeTime);
 
-			// t_max = -p(2) / (2*p(1));
-			//y_max = p(1)*t_max^2 + p(2)*t_max + p(3);
+				if (mbeTimeList.size() > 5)
+				{
+					std::vector<double> diffMbeList;
 
-			// mbe = [mbe; t_max];
+					// Compute the difference list of MBE times
+					for (std::uint32_t kk = 1; kk < mbeTimeList.size(); kk++)
+					{
+						diffMbeList.push_back(mbeTimeList[kk] - mbeTimeList[kk-1]);
+					}
 
-			// if length(mbe) > 1
-				// nextMbe = median(diff(mbe)) + t_max;
-			// else
-				// nextMbe = t_max + 4.61962892466417;
-			//end
+					// Compute the median time of the difference of MBE times
+					std::sort(std::execution::par_unseq, diffMbeList.begin(), diffMbeList.end());
+
+					const double medDiffMbe = diffMbeList[diffMbeList.size()/2];
+
+					std::cout << "Median of diffMbe: " << medDiffMbe << std::endl;
+
+					nextMbeTime = thisMbeTime + medDiffMbe;
+					std::cout << std::setprecision(15) << "Next MBE will be " << nextMbeTime << std::endl;
+				}
+			}
 		}
 
 		packet.numSamples = num_accum_samps;
