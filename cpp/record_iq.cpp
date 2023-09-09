9b4accb7bb239a4ae9e05acc999bb79863c00e66
Chris Wozny <1447143+cwozny@users.noreply.github.com>
2023-09-09 07:41:57 -0400

    bunch of work

--- cpp/record_iq.cpp
+++ cpp/record_iq.cpp
@@ -3,22 +3,40 @@
 */
 #include <libbladeRF.h>
 #include <cstring>
+#include <ctime>
 
 #include <iostream>
+#include <fstream>
+
+const unsigned int sampleLength = 250000;
+const unsigned int bufferSize = 2*sampleLength;
+
+struct IqPacket
+{
+	unsigned int frequencyHz;
+	unsigned int bandwidthHz;
+	unsigned int sampleRate;
+	unsigned int rxGain;
+	int16_t iq[bufferSize];
+};
 
 int main(int argc, char *argv[])
 {
 	int status;
-	struct bladerf *dev = NULL;
-	struct bladerf_devinfo dev_info;
+	bladerf *dev = NULL;
+	bladerf_devinfo dev_info;
+	bladerf_metadata meta;
 	bladerf_channel channel = BLADERF_CHANNEL_RX(0);
+	IqPacket packet;
+	char datetimeStr[80];
+	char filenameStr[80];
 
-	unsigned int frequencyHz = atof(argv[1])*1e6;
-	unsigned int bandwidthHz = atoi(argv[2])*1e6;
-	unsigned int sampleRate = atoi(argv[3])*1e6;
-	int rxGain = atoi(argv[4]);
-	unsigned int sampleLength = 2500000;
-	unsigned int bufferSize = 2*sampleLength;
+	const unsigned int frequencyHz = atof(argv[1])*1e6;
+	const unsigned int requestedBandwidthHz = atoi(argv[2])*1e6;
+	unsigned int receivedBandwidthHz = 0;
+	const unsigned int requestedSampleRate = atoi(argv[3])*1e6;
+	unsigned int receivedSampleRate = 0;
+	const int rxGain = atoi(argv[4]);
 
 	/* Initialize the information used to identify the desired device
 	* to all wildcard (i.e., "any device") values */
@@ -45,28 +63,28 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	status = bladerf_set_sample_rate(dev, channel, sampleRate, NULL);
+	status = bladerf_set_sample_rate(dev, channel, requestedSampleRate, &receivedSampleRate);
 
 	if (status == 0)
 	{
-		std::cout <<  "Sample Rate = " << sampleRate*1e-6 << " Msps" << std::endl;
+		std::cout <<  "Sample Rate = " << receivedSampleRate*1e-6 << " Msps" << std::endl;
 	}
 	else
 	{
-		std::cout <<  "Failed to set samplerate = " << sampleRate << ": " << bladerf_strerror(status) << std::endl;
+		std::cout <<  "Failed to set samplerate = " << requestedSampleRate << ": " << bladerf_strerror(status) << std::endl;
 		bladerf_close(dev);
 		return 1;
 	}
 
-	status = bladerf_set_bandwidth(dev, channel, bandwidthHz, NULL);
+	status = bladerf_set_bandwidth(dev, channel, requestedBandwidthHz, &receivedBandwidthHz);
 
 	if (status == 0)
 	{
-		std::cout << "Bandwidth = " << bandwidthHz*1e-6 << " MHz" << std::endl;
+		std::cout << "Bandwidth = " << receivedBandwidthHz*1e-6 << " MHz" << std::endl;
 	}
 	else
 	{
-		std::cout << "Failed to set bandwidth = " << bandwidthHz << ": " << bladerf_strerror(status) << std::endl;
+		std::cout << "Failed to set bandwidth = " << requestedBandwidthHz << ": " << bladerf_strerror(status) << std::endl;
 		bladerf_close(dev);
 		return 1;
 	}
@@ -74,7 +92,11 @@ int main(int argc, char *argv[])
 	// Disable automatic gain control
 	status = bladerf_set_gain_mode(dev, channel, BLADERF_GAIN_MGC);
 
-	if (status != 0)
+	if (status == 0)
+	{
+		std::cout << "Disabled automatic gain control" << std::endl;
+	}
+	else
         {
                 std::cout << "Failed to disable automatic gain control: " << bladerf_strerror(status) << std::endl;
                 bladerf_close(dev);
@@ -113,37 +135,70 @@ int main(int argc, char *argv[])
 	* interface. SC16 Q11 samples *without* metadata are used. */
 	status = bladerf_sync_config(dev, BLADERF_RX_X1, BLADERF_FORMAT_SC16_Q11, num_buffers, buffer_size, num_transfers, timeout_ms);
 
-	if (status != 0)
+	if (status == 0)
 	{
-		std::cout<< "Failed to configure RX sync interface: " << bladerf_strerror(status) << std::endl;
-		return status;
+		std::cout << "Configured RX sync interface" << std::endl;
 	}
-
-	if (status != 0)
+	else
 	{
+		std::cout << "Failed to configure RX sync interface: " << bladerf_strerror(status) << std::endl;
 		bladerf_close(dev);
 		return 1;
 	}
 
-	int16_t* rx_samples = new int16_t[bufferSize];
-
-	memset(&rx_samples, 0, sizeof(rx_samples));
-
 	status = bladerf_enable_module(dev, BLADERF_RX, true);
 
-	if (status != 0)
+	if (status == 0)
+	{
+		std::cout << "Enabled RX" << std::endl;
+	}
+	else
 	{
 		std::cout << "Failed to enable RX: " << bladerf_strerror(status) << std::endl;
 		bladerf_close(dev);
 	}
+	
+	for(int ii = 0; ii < 10; ii++)
+	{
+		memset(&packet.iq, 0, bufferSize*sizeof(int16_t));
+		
+		packet.frequencyHz = frequencyHz;
+		packet.bandwidthHz = receivedBandwidthHz;
+		packet.sampleRate = receivedSampleRate;
+		packet.rxGain = rxGain;
+	
+		status = bladerf_sync_rx(dev, &packet.iq, sampleLength, &meta, 5000);
 
-	status = bladerf_sync_rx(dev, rx_samples, sampleLength, NULL, 5000);
+		if (status == 0)
+		{
+			std::cout << "Sampling..." << std::endl;
+		}
+		else
+		{
+			std::cout << "Failed to sample: " << bladerf_strerror(status) << std::endl;
+		}
+
+		// current date/time based on current system
+		time_t now = time(0);
+
+		// convert now to UTC time
+		tm *utc = gmtime(&now);
+
+		strftime(datetimeStr, sizeof(datetimeStr), "%Y_%m_%d_%H_%M_%S", utc);
+		snprintf(filenameStr, sizeof(filenameStr), "%s_%04d.iq", datetimeStr, ii);
+
+		std::ofstream fout(filenameStr);
+	    	fout.write((char*)&packet, sizeof(packet));
+	    	fout.close();
+    	}
 
 	status = bladerf_enable_module(dev, BLADERF_RX, false);
-	
-	delete [] rx_samples;
 
-	if (status != 0)
+	if (status == 0)
+	{
+		std::cout << "Disabled RX" << std::endl;
+	}
+	else
 	{
 		std::cout << "Failed to disable RX: " << bladerf_strerror(status) << std::endl;
 	}
