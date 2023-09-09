d73a1d2570a44bbd20071b4fdab2a76b07c7339f
Chris Wozny <1447143+cwozny@users.noreply.github.com>
2023-09-09 07:41:57 -0400

    initial commit

---
+++ convert_iq_to_mat.m
@@ -0,0 +1,61 @@
+%% Clear everything out
+
+clc
+
+fprintf('%s - Clearing everything out\n', datestr(now))
+
+clear all
+close all
+
+%% Load data
+
+listing = dir;
+
+for ii = 1:length(listing)
+
+    if contains(listing(ii).name,'.bin')
+
+        fprintf('%s - Reading %s\n', datestr(now), listing(ii).name)
+
+        fileinfo = sscanf(listing(ii).name,"%dM_%d_MHz_%d.bin");
+
+        fs = fileinfo(1)*1e6;
+        fc = fileinfo(2);
+        
+        fid = fopen(listing(ii).name,"r");
+        
+        iq = fread(fid,[2,inf],'float32=>float32');
+%         iq = fread(fid,[2,10],'float32=>float32');
+        
+        fclose(fid);
+        
+        %% Convert
+        
+        fprintf('%s - Convert to I/Q\n', datestr(now))
+        
+        iq = iq(1,:) + 1j*iq(2,:);
+
+        %% Compute duration
+
+        dur = length(iq)/fs;
+        
+        %% Saving data
+        
+        fprintf('%s - Saving I/Q\n', datestr(now))
+        
+        [filepath,name,ext] = fileparts(listing(ii).name);
+        
+        save(sprintf('%s.mat',name),'iq','fs','fc','dur','-v7.3')
+        
+        clear iq
+        clear fs
+        clear fc
+        clear dur
+        
+        %% Done
+        
+        fprintf('%s - Done\n', datestr(now))
+
+    end
+
+end
---
+++ cpp/record_iq.cpp
@@ -0,0 +1,191 @@
+/* Save to a file, e.g. boilerplate.c, and then compile:
+* $ gcc boilerplate.c -o libbladeRF_example_boilerplate -lbladeRF
+*/
+#include <libbladeRF.h>
+#include <cstring>
+
+#include <complex>
+#include <vector>
+#include <iostream>
+
+/* The RX and TX channels are configured independently for these parameters */
+struct channel_config
+{
+	bladerf_channel channel;
+	unsigned int frequency;
+	unsigned int bandwidth;
+	unsigned int samplerate;
+	int gain;
+};
+
+static int init_sync(struct bladerf *dev)
+{
+	int status;
+	/* These items configure the underlying asynch stream used by the sync
+	* interface. The "buffer" here refers to those used internally by worker
+	* threads, not the user's sample buffers.
+	*
+	* It is important to remember that TX buffers will not be submitted to
+	* the hardware until `buffer_size` samples are provided via the
+	* bladerf_sync_tx call. Similarly, samples will not be available to
+	* RX via bladerf_sync_rx() until a block of `buffer_size` samples has been
+	* received. */
+	const unsigned int num_buffers = 16;
+	const unsigned int buffer_size = 8192; /* Must be a multiple of 1024 */
+	const unsigned int num_transfers = 8;
+	const unsigned int timeout_ms = 3500;
+
+	/* Configure both the device's x1 RX and TX channels for use with the
+	* synchronous
+	* interface. SC16 Q11 samples *without* metadata are used. */
+	status = bladerf_sync_config(dev, BLADERF_RX_X1, BLADERF_FORMAT_SC16_Q11, num_buffers, buffer_size, num_transfers, timeout_ms);
+
+	if (status != 0)
+	{
+		std::cout<< "Failed to configure RX sync interface: " << bladerf_strerror(status) << std::endl;
+		return status;
+	}
+
+	status = bladerf_sync_config(dev, BLADERF_TX_X1, BLADERF_FORMAT_SC16_Q11, num_buffers, buffer_size, num_transfers, timeout_ms);
+
+	if (status != 0)
+	{
+		std::cout << "Failed to configure TX sync interface: " << bladerf_strerror(status) << std::endl;
+	}
+
+	return status;
+}
+
+int configure_channel(struct bladerf *dev, struct channel_config *c)
+{
+	int status = bladerf_set_frequency(dev, c->channel, c->frequency);
+
+	if (status != 0)
+	{
+		std::cout << "Failed to set frequency = " << c-> frequency << ": " << bladerf_strerror(status) << std::endl;
+		return status;
+	}
+
+	status = bladerf_set_sample_rate(dev, c->channel, c->samplerate, NULL);
+
+	if (status != 0)
+	{
+		std::cout <<  "Failed to set samplerate = " << c->samplerate << ": " << bladerf_strerror(status) << std::endl;
+		return status;
+	}
+
+	status = bladerf_set_bandwidth(dev, c->channel, c->bandwidth, NULL);
+
+	if (status != 0)
+	{
+		std::cout << "Failed to set bandwidth = " << c->bandwidth << ": " << bladerf_strerror(status) << std::endl;
+		return status;
+	}
+
+	// Disable automatic gain control
+	status = bladerf_set_gain_mode(dev, c->channel, BLADERF_GAIN_MGC);
+
+	if (status != 0)
+        {
+                std::cout << "Failed to disable automatic gain control: " << bladerf_strerror(status) << std::endl;
+                return status;
+        }
+
+	status = bladerf_set_gain(dev, c->channel, c->gain);
+
+	if (status != 0)
+	{
+		std::cout << "Failed to set gain: " << bladerf_strerror(status) << std::endl;
+		return status;
+	}
+
+	return status;
+}
+
+#define SAMPLE_RATE 50e6 // each sample represents 20 ns
+#define SAMPLE_LENGTH 1048576 // each buffer represents 20.972 milliseconds
+#define INT16_BUFFER_LENGTH 2097152
+
+int main(int argc, char *argv[])
+{
+	int status;
+	struct channel_config config;
+	struct bladerf *dev = NULL;
+	struct bladerf_devinfo dev_info;
+
+	/* Initialize the information used to identify the desired device
+	* to all wildcard (i.e., "any device") values */
+	bladerf_init_devinfo(&dev_info);
+
+	status = bladerf_open_with_devinfo(&dev, &dev_info);
+
+	if (status != 0)
+	{
+		std::cout << "Unable to open device: " << bladerf_strerror(status) << std::endl;
+		return 1;
+	}
+
+	/* Set up RX channel 1 parameters */
+	config.channel = BLADERF_CHANNEL_RX(0);
+	config.frequency = 152.6e6;
+	config.bandwidth = 1e6;
+	config.samplerate = SAMPLE_RATE;
+	config.gain = 60;
+
+	status = configure_channel(dev, &config);
+
+	if (status != 0)
+	{
+		std::cout << "Failed to configure RX channel 1. Exiting." << std::endl;
+		bladerf_close(dev);
+		return 1;
+	}
+
+	/* Initialize synch interface on RX and TX */
+	status = init_sync(dev);
+
+	if (status != 0)
+	{
+		bladerf_close(dev);
+		return 1;
+	}
+
+	int16_t rx_samples[INT16_BUFFER_LENGTH]; // Receive sample buffer
+	std::vector<std::complex<float>> iq;
+
+	memset(&rx_samples, 0, sizeof(rx_samples));
+
+	status = bladerf_enable_module(dev, BLADERF_RX, true);
+
+	if (status != 0)
+	{
+		std::cout << "Failed to enable RX: " << bladerf_strerror(status) << std::endl;
+		bladerf_close(dev);
+	}
+
+	status = bladerf_sync_rx(dev, rx_samples, SAMPLE_LENGTH, NULL, 5000);
+
+	// Convert data from int16 I/Q to single precision I/Q
+	for(int i = 0; i < INT16_BUFFER_LENGTH; i += 2)
+	{
+		std::complex<float> complex_sample(rx_samples[i], rx_samples[i+1]);
+
+		complex_sample /= 2048.0; // normalize sample
+
+		iq.push_back(complex_sample);
+	}
+
+	for(int n = 0; n < iq.size(); n++)
+	{
+		std::cout << std::real(iq[n]) << "," << std::imag(iq[n]) << std::endl;
+	}
+
+	status = bladerf_enable_module(dev, BLADERF_RX, false);
+
+	if (status != 0)
+	{
+		std::cout << "Failed to disable RX: " << bladerf_strerror(status) << std::endl;
+	}
+
+	return status;
+}
