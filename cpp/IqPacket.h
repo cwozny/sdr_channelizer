bb4088535fa6931fc723e871bdb5a34fad41c7d1
Woz <1447143+cwozny@users.noreply.github.com>
2023-09-10 20:59:36 -0400

    Add bitwidth to file
    Added a field to packet header so we know what the bitwidth of the IQ samples are

--- cpp/IqPacket.h
+++ cpp/IqPacket.h
@@ -5,17 +5,17 @@
 
 struct IqPacket
 {
-	std::uint32_t endianness;
-	std::uint32_t linkSpeed;
-	std::uint32_t frequencyHz;
-	std::uint32_t bandwidthHz;
-	std::uint32_t sampleRate;
-	std::uint32_t rxGain;
-	std::uint32_t numSamples;
-	std::uint32_t spare1;
-	char fpgaVersion[32];
-	char fwVersion[32];
-	double sampleStartTime;
+  std::uint32_t endianness;
+  std::uint32_t linkSpeed;
+  std::uint32_t frequencyHz;
+  std::uint32_t bandwidthHz;
+  std::uint32_t sampleRate;
+  std::uint32_t rxGain;
+  std::uint32_t numSamples;
+  std::uint32_t bitWidth;
+  char fpgaVersion[32];
+  char fwVersion[32];
+  double sampleStartTime;
 };
 
 #endif
